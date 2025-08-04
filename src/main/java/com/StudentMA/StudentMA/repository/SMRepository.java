9a78a9f79b900558d138f3e5fb349df5daf82b14
Pooja Kothawade <126547158+pooja123-kothawade@users.noreply.github.com>
2025-08-04 23:18:17 +1400

    Delete src directory

--- src/main/java/com/StudentMA/StudentMA/repository/SMRepository.java
+++
@@ -1,9 +0,0 @@
-package com.StudentMA.StudentMA.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.StudentMA.StudentMA.model.SMModel;
-
-public interface SMRepository extends JpaRepository<SMModel, Long> {
-
-}
